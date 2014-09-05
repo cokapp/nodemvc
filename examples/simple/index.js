21b4c87fe1ec6f8feb959c2097dd00396c910b20
cokapp <office@cokapp.com>
2014-09-05 17:52:29 +0800

    Signed-off-by: cokapp <office@cokapp.com>

---
+++ examples/simple/index.js
@@ -0,0 +1,9 @@
+//Set Core Lib
+var NodeMVC = require('../../core/NodeMVC');
+//Set AppRoot
+NodeMVC.setAppRoot('./');
+//StartUP
+NodeMVC.startup(function(server){
+	//NodeMVC is working, enjoy!
+});
+
